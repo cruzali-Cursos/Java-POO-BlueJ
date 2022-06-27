ad6d88dce3c1573ad3de6ea0409fba299345cee6
Ali Cruz <ali.cruz@me.com>
2022-06-26 22:48:58 -0500

    Pag 54 Ejercicio 2.77

--- Ejercicios/ejercicio-libro-2.74/Libro.java
+++ Ejercicios/ejercicio-libro-2.74/Libro.java
@@ -10,14 +10,28 @@ public class Libro
     // instance variables - replace the example below with your own
     private String autor;
     private String titulo;
+    private int paginas;
+    private String numeroDeReferencia;
 
     /**
      * Constructor for objects of class Libro
      */
-    public Libro(String autorDelLibro, String tituloDelLibro)
+    public Libro(String autorDelLibro, String tituloDelLibro, int paginasDelLibro, String numeroDeReferenciaDelLibro)
     {
         autor = autorDelLibro;
         titulo = tituloDelLibro;
+        paginas = paginasDelLibro;
+        numeroDeReferencia = numeroDeReferenciaDelLibro;
+    }
+    
+    public void setNumeroDeReferencia(String ref)
+    {
+        numeroDeReferencia = ref;
+    }
+    
+    public void getNumeroDeReferencia()
+    {
+        System.out.print("\nEl numero de referencia es: " + numeroDeReferencia);
     }
     
     public String getAutor() {
@@ -27,6 +41,30 @@ public class Libro
     public String getTitulo() {
         return titulo;
     }
+    
+    public int getPaginas()
+    {
+        return paginas;
+    }
 
+    public void imprimirAutor()
+    {
+        System.out.print("\n El autor es: " + autor);
+    }
+    
+    public void imprimirLibro()
+    {
+        System.out.print("\n El titulo es: " + titulo);
+    }
+    
+    public void imprimePaginas()
+    {
+        System.out.print("\n Numero de paginas: " + paginas);
+    }
+    
+    public void imprimirDetalles()
+    {
+        System.out.print("\nDetalles del libro ::: \nAutor: " + autor + "\nTitulo: " + titulo + "\nPaginas: " + paginas);
+    }
   
 }
