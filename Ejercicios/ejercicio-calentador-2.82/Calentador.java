98301551076e996f132108c19795196d696aa79f
Ali Cruz <ali.cruz@me.com>
2022-07-08 18:49:24 -0500

    update

--- Ejercicios/ejercicio-calentador-2.82/Calentador.java
+++ Ejercicios/ejercicio-calentador-2.82/Calentador.java
@@ -9,31 +9,56 @@ public class Calentador
 {
     // instance variables - replace the example below with your own
     private int temperatura;
+    private int min;
+    private int max;
+    private int incremento;
 
     /**
      * Constructor for objects of class Calentador
      */
-    public Calentador()
+    public Calentador(int miMin, int miMax)
     {
         temperatura = 15;
         // initialise instance variables
+        min = miMin;
+        max = miMax;
+        incremento = 5;
     }
     
     public void calentar()
     {
-        //
-        temperatura = temperatura + 5;
+        if ((temperatura + incremento) > max) {
+            System.out.println("No se puede subir a mas de " + max);
+        } else {
+            temperatura += incremento;
+            System.out.println("\nSube temperatura: " + temperatura);
+        }
+        
     }
     
-    public void engriar()
+    public void enfriar()
     {
-        //
-        temperatura = temperatura - 5;
+        if ((temperatura - incremento) < min) {
+            System.out.println("No se puede bajar a menos de " + min);
+        } else {
+            temperatura -= incremento;
+            System.out.println("\nBaja temperatura: " + temperatura);
+        }
+        
     }
     
     public int getTemperatura()
     {
         return temperatura;
+    }
+    
+    public void setIncremento(int miIncremento)
+    {
+        if (miIncremento <= 0) {
+            System.out.println("\nSolo se aceptan valores mayores a 0");
+        } else {
+            incremento = miIncremento;
+        }
     }
 
     /**
